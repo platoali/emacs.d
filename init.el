efcc19a82395a24639237f7342686ffa2cb766b8
Ali A. Yazdi <platoali@gmail.com>
2023-09-23 12:34:25 +0330

    some minor bugs after merging.

--- init.el
+++ init.el
@@ -12,43 +12,6 @@
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
 
-<<<<<<< HEAD
-=======
-;;(load-file "~/.emacs.d/myinit.el")
-;; ido
-
-;; (setq indo-eable-flex-matching t)
-;; (setq ido-everywhere t)
-;; (ido-mode 1)
-
-;; wind ace
-
-;; (use-package ace-window
-;;   :ensure t
-;;   :init
-;;   (progn
-;;     (global-set-key [remap other-window] 'ace-window)
-;;     (
-;;     ))
-
-
-;; (use-package auto-complete
-;;   :ensure t
-;;   :init
-;;   (progn
-;;     (ac-config-default)
-;;     (global-auto-complete-mode t)
-;;    ))
-
-
- 
-
-
->>>>>>> help
-(put 'narrow-to-region 'disabled nil)
-(put 'narrow-to-page 'disabled nil)
-
-
 (provide 'init)
 ;;; init.el ends here
 
